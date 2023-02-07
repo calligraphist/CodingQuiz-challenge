2ca8300e04f84a8597fd73dcd9b7c06d537b8713
“anar <sehar.uzair23@gmail.com>
2023-02-07 10:56:01 -0400

    make questions appear and answers with buttons

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 //define variables and assigning values using web APIs 
 var startBtn =document.getElementById("startTime")
 console.log(startBtn)
+var qi=0
 var timerEl=document.getElementById("timer-value")
+var scoreEl=document.getElementById("score-value")
+var score=0
+scoreEl.textContent=score
 var timeleft=75
 var timeInterval;
 function startGame(){
     document.getElementById("start-screen").style.display="none"
     document.getElementById("quiz-screen").style.display="block"
     timeInterval=setInterval(reducetime, 1000)
+    askquestion()
+}
+function askquestion(){
+    document.getElementById("question-text").textContent=questions[qi].title
+    document.getElementById("answer-box").innerHTML=""
+    questions[qi].choices.forEach(function(choice){
+        console.log(choice);
+        var button= document.createElement("button");
+        button.textContent=choice;
+        button.setAttribute("value", choice)
+        button.onclick=function(){
+           if(this.value !== questions[qi].answer){
+            timeleft -= 15
+           }
+           else{
+            score++
+            scoreEl.textContent=score;
+           }
+           qi++;
+           if(qi===questions.length){
+            //call end game here.
+           }
+           else{
+            askquestion()
+           }
+           askquestion();
+        }
+        document.getElementById("answer-box").append(button);
+    })
 }
 // Timer that counts down from 75
 function reducetime(){
@@ -17,8 +50,9 @@ function reducetime(){
         timerEl.textContent=timeleft
     }
 }
+
 //defining questions, options and answers
-// questions *****************************************************
+
 var questions = [
     {
         title: "Commonly Used data types DO NOT include:",
